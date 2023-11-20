1b7643bddc8c9f58e795b557aedf8b20657432fb
Tiffany GIBIER <tgibier@made-f0Br5s7.clusters.42paris.fr>
2023-11-20 18:04:23 +0100

    fixed 01/ex04

--- CPP01/ex04/Stringz.cpp
+++ CPP01/ex04/Stringz.cpp
@@ -12,24 +12,32 @@ void    replaceLine(std::string& line, std::string s1, std::string s2)
 
 }
 
-int    replaceStrings(std::string infileName, std::string s1, std::string s2) {
-
+int    replaceStrings(std::string infileName, std::string s1, std::string s2)
+{
 	std::string outfileName;
-	std::string	line;
 
 	outfileName = infileName + ".replace";
    
 	std::ifstream ifs(infileName.c_str());
+	std::ofstream ofs(outfileName.c_str());
 
 	if (ifs.peek() == std::ifstream::traits_type::eof())
-		return (std::cout << "Error with that infile :(" << std::endl, 1);
-	std::ofstream ofs(outfileName.c_str());
-	if (!ofs.is_open())
+		return (std::cout << "Your infile is empty :(" << std::endl, 1);
+	if (!ifs.is_open() || !ofs.is_open())
 		return (std::cout << "Can't open filezzz" << std::endl, 1);
-	while (std::getline(ifs, line))
+	
+
+    std::ostringstream buffer;
+	buffer << ifs.rdbuf();
+    std::string	line = buffer.str();
+
+	size_t i = 0;
+
+    while ((i = line.find(s1, i)) != std::string::npos)
 	{
-		replaceLine(line, s1, s2);
-		ofs << line << std::endl;
+		line = line.substr(0, i) + s2 + line.substr(i + s1.length());
+		i += s2.length();
 	}
+	ofs << line;
 	return 0;
 }
--- CPP01/ex04/Stringz.hpp
+++ CPP01/ex04/Stringz.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 
 int    replaceStrings(std::string infileName, std::string s1, std::string s2);
